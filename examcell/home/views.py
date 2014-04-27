3090d3224612692fd898ed3e827ac7453572a7e3
Ratanraj Ravuri <ratanraj.r@gmail.com>
2014-04-27 15:39:37 +0530

    validations in registration form

--- examcell/home/views.py
+++ examcell/home/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
-
-# Create your views here.
+from django.core.context_processors import csrf
+from student.models import Student
+from django.http.response import HttpResponseRedirect
+from django.forms.models import ModelForm
 
 @login_required
 def home(request):
 	return render_to_response("home.html",{})
 
-def login(request):
-	pass
+class StudentRegistrationForm(ModelForm):
+	class Meta:
+		model=Student
+
+
+def register(request):
+	if request.method=='GET':
+		c={}
+		c.update(csrf(request))
+		c.update({'form':StudentRegistrationForm()})
+		return render_to_response("registration/register.html",c)
+	
+	if request.method=='POST':
+		form=StudentRegistrationForm(request.POST,request.FILES)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect('/apply/')
+		
+	
