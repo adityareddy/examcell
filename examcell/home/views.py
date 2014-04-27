c3ad6f580f67fe659aaed765a254f22079725bb0
Ratanraj Ravuri <ratanraj.r@gmail.com>
2014-04-27 18:36:23 +0530

    User Roles. changed the home.html to StudentHome.html 	new file:   home/templates/StudentHome.html Added the ExamCellHome.html for the ExamCell Control panel 	new file:   home/templates/ExamCellHome.html slightly modified the mainbase.html template 	modified:   home/templates/mainbase.html
    Added user roles
    	modified:   home/views.py

--- examcell/home/views.py
+++ examcell/home/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.core.context_processors import csrf
 from student.models import Student
-from django.http.response import HttpResponseRedirect
+from django.http.response import HttpResponseRedirect, HttpResponse
 from django.forms.models import ModelForm
 
 @login_required
 def home(request):
-	return render_to_response("home.html",{})
+	group = request.user.groups.values_list('name')[0][0]
+	if group == 'Student':
+		return render_to_response("StudentHome.html",{'user':request.user,'group':group})
+	elif group == 'Department':
+		return render_to_response("DepartmentHome.html",{'user':request.user,'group':group})
+	elif group == 'ExamCell':
+		return render_to_response("ExamCellHome.html",{'user':request.user,'group':group})
+	else:
+		return HttpResponse('Invalid User')
 
 class StudentRegistrationForm(ModelForm):
 	class Meta:
